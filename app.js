a085fe749b032d708c15e434beaf093e8871e3e3
Jarne-Drijbooms <r0781657@student.thomasmore.be>
2023-02-03 09:00:39 +0100

    security

--- app.js
+++ app.js
@@ -4,13 +4,46 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+const crypto = require("crypto")
+const algorithm = "aes-256-cbc"
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
+//var sql = require("mssql");
+const { COnnection, Request, Connection } = require("tedious");
+
+const dotenv = require('dotenv');
+
+function encrypt(text){
+    let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex')};
+}
+
+function decrypt(text){
+    let iv = Buffer.from(text.iv, 'hex');
+    let encryptedText = Buffer.from(text.encryptedData, 'hex');
+    let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
+
+
+
+
 var app = express();
 
 //Routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var phoneDetectionRouter = require('./routes/phone_detection');
-var droneDetectionRouter = require('./routes/drone_detection');
+const { buffer } = require('@tensorflow/tfjs');
+// // var flowersRouter = require('./routes/flowers');
 
 
 app.use(express.json());
@@ -33,9 +66,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-app.use('/phone-big-img', phoneDetectionRouter);
-app.use('/drone-big-img', droneDetectionRouter);
+app.use('/image', phoneDetectionRouter);
+// // app.use('/flowers', flowersRouter); 
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
@@ -61,43 +93,91 @@ app.use('/drone-big-img', droneDetectionRouter);
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+/////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//Testing split image
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//JWTtoken code
+app.get('/api', (req, res) => {
+    res.json({
+        message: "Welcome to the API!"
+    });
+});
+
+app.post('/api/posts', verifyToken, (req, res) => {
+    jwt.verify(req.token, 'secretkey', (err, authData)=>{
+        if(err){
+            res.sendStatus(403);
+        }else{
+            res.json({
+                message: "Post created ...",
+                authData
+            });
+        }
+    });
+});
+
+
+app.post("/api/login", (req, res)=> {
+    const user = {
+        id: 1,
+        username: 'User01',
+        email: 'user01@hotmail.com',
+    }
+
+    jwt.sign({user}, 'secretkey',{expiresIn: '30m' }, (err, token)=>{
+        res.json({
+            token
+        });
+    });
+});
 
 
 
+function verifyToken(req, res, next){
+    const bearerHeader = req.headers["authorization"];
+    if(typeof bearerHeader !== 'undefined'){
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+
+    } else{
+        res.sendStatus(403);
+    }
+};
 
 
+const dbconfig ={
+    authentication: {
+        options: {
+            userName: "dev6",
+            password: "n7Vw24vSzsyMKL"
+        },
+        type: "default"
+    },
+    server: "dev6h3.database.windows.net",
+    options: {
+        database: "dev6h3",
+        encrypt: true
+    }
+};
+
+const connection = new Connection(dbconfig);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+connection.on("connect", err => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      queryDatabase();
+    }
+  });
 
 
 
